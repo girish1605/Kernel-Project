b36c9ae53bb76db4a92e4fd7d8ec8264e5b40d6f
girish1605 <girish.1605@gmail.com>
2017-01-21 09:18:27 +0530

    Server Client setup for sysctl (NOT WORKING)(Jan 21, 2017)

--- clientDir/client.c
+++ clientDir/client.c
@@ -7,33 +7,63 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <linux/sysctl.h>
+#include <sys/syscall.h>
 
 #include "../configDir/connection.h"
 
-int main(int argc, char* argv[]) {
+#define STRING_MAX 100
 
+//int _sysctl(struct __sysctl_args * args);
+
+void update_kern_para(int sockfd) {
+	struct __sysctl_args args;
+	int retval;
+	char oldval[STRING_MAX];
+	size_t oldlen;
+	
+	memset(oldval, '\0', STRING_MAX);
+//	memset(&args, 0, sizeof(struct __sysctl_args));
+	retval = recv(sockfd, &args, 1024, 0);
+	CHECK_ERROR((retval < 0), "Receive");	
+
+	args.oldval = oldval;
+	args.oldlenp = &oldlen;
+
+	printf("name[0] = %d\n", args.name[0]);
+	printf("name[1] = %d\n", args.name[1]);
+
+	retval = syscall(SYS__sysctl, &args);
+	CHECK_ERROR((retval == -1), "_sysctl");
+	
+	printf("oldval = %s | oldlen = %lu\n\
+		newval = %s | newlen = %lu\n", oldval, oldlen, (char *)args.newval, args.newlen);
+}
+
+int main(int argc, char* argv[]) {
+	char *ip = "127.0.0.1";
 	int sockfd, retval;
 	struct sockaddr_in servAddr;
 	socklen_t servLen;
 	char buff[BUFSIZE];
-	if(argv[1] == NULL) {
+/*	if(argv[1] == NULL) {
 		printf("Invalid network address \"%s\"\n", argv[1]);
 		exit(1);
 	}
-
+*/
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	CHECK_ERROR((sockfd < 0), "Socket");
 
 	bzero(&servAddr, sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htons(PORT_NUMBER);
-	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_addr.s_addr = inet_addr(ip);
 
 	servLen = sizeof(servAddr);
 	retval = connect(sockfd, (struct sockaddr *)&servAddr, servLen);
 	CHECK_ERROR((retval < 0), "Connect");
 
-	do {
+/*	do {
 		memset(buff, '\0' , BUFSIZE);
 		retval = recv(sockfd, buff, BUFSIZE, 0);
                 printf("Computer :: %s\n", buff);
@@ -43,8 +73,10 @@ int main(int argc, char* argv[]) {
 		scanf(" %[^\n]", buff);
 		retval = send(sockfd, buff, strlen(buff), 0);
 	} while(1);
-
+*/
 	printf("connection establisted\n");
+
+	update_kern_para(sockfd);	
 
 	return 0;
 }
--- serverDir/server.c
+++ serverDir/server.c
@@ -13,7 +13,41 @@
 
 #define MAX_CONNECTION 5
 #define CHILD 0
+#define STRING_MAX 100
 
+void send_update_kern_para(int connfd) {
+
+	struct __sysctl_args args;
+		
+	int name[] = { CTL_KERN, KERN_NODENAME };
+	int name_len = sizeof(name)/sizeof(name[0]);
+//	char oldval[STRING_MAX];
+//	size_t oldlen;
+	char newval[] = "GirishB";
+	size_t newlen;
+	
+	int retval;
+	
+/*	memset(newval, '\0', STRING_MAX);
+	printf("Enter New value :: ");
+	scanf(" %s", newval);
+*/
+	newlen = strlen(newval);
+
+	memset(&args, 0, sizeof(struct __sysctl_args));
+	args.name = name;
+	args.nlen = name_len;
+//	args.oldval = oldval;
+//	args.oldlenp = &oldlen;
+	args.newval = newval;
+	args.newlen = newlen;
+	
+	printf(" %d %d\n", args.name[0], args.name[1]);
+	
+	retval = send(connfd, &args, sizeof(args), 0);
+	CHECK_ERROR((retval < 0), "Send");
+}
+	
 int main(int argv, char* argc[])
 {
 	int sockfd, connfd, retval;
@@ -45,10 +79,10 @@ int main(int argv, char* argc[])
 		connfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
 		CHECK_ERROR((connfd < 0), "Accept");		
 
-		pid = fork();
-		if(pid == CHILD) {
+//		pid = fork();
+//		if(pid == CHILD) {
 			printf("+1 is Connnected\n");	
-			while (1) {
+		/*	while (1) {
 				memset(buff, '\0', BUFSIZE);
                                 printf("Me :: ");
                                 scanf(" %[^\n]", buff);
@@ -56,11 +90,11 @@ int main(int argv, char* argc[])
 
 				memset(buff, '\0', BUFSIZE);
 				retval = recv(connfd, buff, BUFSIZE, 0);
-				printf("User ::  %s\n", buff);
-			
-				
+				printf("User ::  %s\n", buff);	
 			}
-		}
+		*/
+			send_update_kern_para(connfd);
+//		}
 	} while(1);
 	return 0;
 }
